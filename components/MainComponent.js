adafe608347064c17a8590cf2efd74d988671bb6
Vishal Nawale <vishal.nawale@ltts.com>
2019-10-31 16:01:23 +0530

    react navigation part2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import Menu from './MenuComponent';
+import Home from './homecomponent';
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from "react-native";
-import {createStackNavigator} from 'react-navigation-stack'
+import {createStackNavigator } from 'react-navigation-stack';
+import {createDrawerNavigator} from 'react-navigation-drawer';
 import { createAppContainer } from "react-navigation";
 
 
@@ -24,7 +26,39 @@ const MenuNavigator = createStackNavigator({
 }
 );
 
-const AppContainer=createAppContainer(MenuNavigator);
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff"  
+    })
+});
+
+const MainNavigator = createDrawerNavigator({
+    Home: 
+      { screen: HomeNavigator,
+        navigationOptions: {
+          title: 'Home',
+          drawerLabel: 'Home'
+        }
+      },
+    Menu: 
+      { screen: MenuNavigator,
+        navigationOptions: {
+          title: 'Menu',
+          drawerLabel: 'Menu'
+        }, 
+      }
+}, {
+  drawerBackgroundColor: '#D1C4E9'
+});
+const AppContainer=createAppContainer(MainNavigator);
  class Main extends React.Component{
     
 
