da4bc22b7328f510b88587bc68cb6b14e9f43bbd
Vishal Nawale <vishal.nawale@ltts.com>
2019-11-06 12:44:19 +0530

    Icons and customs draweritems

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, View ,ScrollView,FlatList} from 'react-native';
+import { Card,Icon } from 'react-native-elements';
 import { DISHES } from './shared/dishes';
+import { COMMENTS } from './shared/comments';
 
 function RenderDish(props) {
 
@@ -15,6 +16,14 @@ function RenderDish(props) {
                     <Text style={{margin: 10}}>
                         {dish.description}
                     </Text>
+                    <Icon 
+                    raised
+                    reverse
+                    name={props.favorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    onPress={()=>props.faourites ? console.log("Already favourite") : props.onPress()
+                    }/>
                 </Card>
             );
         }
@@ -23,14 +32,43 @@ function RenderDish(props) {
         }
 }
 
+function RenderComments(props){
+    const comments=props.comments;
+    const rendercommentItem=({item,index})=>{
+
+        return(
+            <View key={index} style={{margin:10}}>
+                <Text style={{fontSize:14}}>{item.comment}</Text>
+                <Text style={{fontSize:12}}>{item.rating}</Text>
+                <Text style={{fontSize:12}}>{'--'+item.author+', '+item.date}</Text>
+            </View>
+        );
+    }
+
+    return(
+
+        <Card title="Comments">
+            <FlatList data={comments}
+            renderItem={rendercommentItem}
+            keyExtractor={item =>item.id.toString()}/>
+        </Card>
+    );
+}
+
 class  Dishdetail extends Component {
 
     constructor(props){
         super(props);
         this.state={
-            dishes:DISHES
+            dishes:DISHES,
+            comments:COMMENTS,
+            faourites:[]
         };
     };
+    markFaourites(dishId){
+        this.setState({faourites:this.state.faourites.concat(dishId)})
+
+    }
 
     static navigationOptions = {
         title: 'Dish Details',
@@ -45,7 +83,14 @@ class  Dishdetail extends Component {
     render() {
         const dishId = this.props.navigation.getParam('dishId','');
         return(
-            <RenderDish dish={this.state.dishes[+dishId]} />
+            <ScrollView >
+            <RenderDish dish={this.state.dishes[+dishId]}
+            favorite={this.state.faourites.some(el => el ===dishId)} 
+            onPress={()=>this.markFaourites(dishId)}
+            />
+            <RenderComments comments={this.state.comments.filter((comment) => comment.dishId === dishId)} />
+
+            </ScrollView>
         );
     }
 }
